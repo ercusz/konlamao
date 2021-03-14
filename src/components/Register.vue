3e32c263c5e23f6dfb2a1a8b4fc1ddbf08e3441f
ercusz <eurokungz.xkz@gmail.com>
2021-03-14 18:35:56 +0700

    abcdefg

---
+++ src/components/Register.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <!-- Content goes here -->
+            <h1>Create Student</h1>
+            <form @submit.prevent="handleSubmitForm">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="phone">Phone</label>
+                    <input type="text" class="form-control" required>
+                </div>
+
+                <div class="form-group">
+                    <button class="btn btn-success btn-block">Create</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default ({
+    data() {
+        return{
+
+        }
+    },
+})
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Home from "../views/Home.vue";
+import Register from "@/components/Register.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -15,7 +16,13 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: Register,
   }
+
 ];
 
 const router = createRouter({
